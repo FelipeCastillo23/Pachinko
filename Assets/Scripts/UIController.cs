d7a69d620d0b1dbcb137148911d20e2548849bfa
Felipe Castillo <fcp323@gmail.com>
2024-02-20 21:24:01 -0500

    Implemented basic main menu
    Using UI Toolkit to create a basic main menu.
    - Start Game button working
    
    TODO:
    - Option button functionality
    - Quit button functionality
    
    Followed this tutorial https://www.youtube.com/watch?v=NQYHIH0BJbs

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public Button startButton;
+    public Button optionButton;
+    public Button quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        startButton = root.Q<Button>("StartBtn");
+        optionButton = root.Q<Button>("OptionsBtn");
+        quitButton = root.Q<Button>("QuitBtn");
+
+        startButton.clicked += StartBtnPressed;
+        optionButton.clicked += OptionBtnPressed;
+        quitButton.clicked += QuitBtnPressed;
+    }
+
+
+
+    void StartBtnPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void OptionBtnPressed()
+    {
+        Debug.Log("Option Button Pressed");
+    }
+
+    void QuitBtnPressed()
+    {
+        Debug.Log("Quit Button Pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
